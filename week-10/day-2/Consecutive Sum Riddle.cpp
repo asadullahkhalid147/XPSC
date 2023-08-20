c259070e1300b941f40725ca5191ac68cd66a5e3
asadullahkhalid147 <asadullahkhalid147@gmail.com>
2023-08-20 23:40:44 +0600

    week-10 day-2

---
+++ week-10/day-2/Consecutive Sum Riddle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll t;cin>>t;
+    while(t--)
+    {
+        ll n;cin>>n;
+        cout<<-(n-1)<<" "<<n<<endl;
+    }
+}
